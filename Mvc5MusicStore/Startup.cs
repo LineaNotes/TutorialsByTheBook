90e88e16b61ceb7536c1737d28b116d4572c05f8
LineaNotes <anze.premrl@hotmail.com>
2016-03-29 20:52:09 +0200

    Initial commit - MVC 5 with Individual accounts and test files

---
+++ Mvc5MusicStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc5MusicStore.Startup))]
+namespace Mvc5MusicStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
